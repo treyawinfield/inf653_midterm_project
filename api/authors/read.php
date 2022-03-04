2df7f198d3e6879621f8b68088be226085cb5769
Treya Winfield <you@example.com>
2022-03-03 22:25:30 -0800

    Updated read.php

--- api/authors/read.php
+++ api/authors/read.php
@@ -3,8 +3,6 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
-    include_once '../authors/index.php';
-
     include_once '../../config/Database.php';
     include_once '../../models/Author.php';
 
