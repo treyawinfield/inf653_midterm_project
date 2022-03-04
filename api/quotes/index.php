b0e281ef37d6f86255e93cc45690014de8d3828d
Treya Winfield <you@example.com>
2022-03-03 22:49:18 -0800

    Updated api

--- api/quotes/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    }
-?>
