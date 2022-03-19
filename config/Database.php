9bf77791f76b5c755d1e6c8b11b6fda5841c33d9
Treya Winfield <you@example.com>
2022-03-19 13:16:18 -0700

    Modified Database config

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,6 @@
 <?php 
     class Database {
         // Private DB Params, can only be accessed inside class
-        /*
         private $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $db_name = 'sh8c38icjzsz9w7t';
         private $username = 'y7m11tf4hkiuh8sd';
@@ -12,13 +11,12 @@
         public function __construct() {
             $this->password = getenv('DB_PASSWORD');
         }
-        */
         
-        private $host = 'localhost';
-        private $db_name = 'quotesdb';
-        private $username = 'root';
-        private $password = '';
-        private $conn;
+        // private $host = 'localhost';
+        // private $db_name = 'quotesdb';
+        // private $username = 'root';
+        // private $password = '';
+        // private $conn;
         
         
         // DB Connect Method
